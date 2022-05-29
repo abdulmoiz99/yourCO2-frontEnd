c69fa582394bb959cee6ad77e773eec7d65ac9cd
Abdul Moiz <moiza0799@gmail.com>
2022-05-29 10:55:44 +0500

    Ignore

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 // import './index.css';
-import * as ReactDOMClient from 'react-dom/client';
+import * as ReactDOMClient from 'react-dom/client'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 // import App from './App';
 import reportWebVitals from './reportWebVitals'
 import Index from './views/Index'
-import {Login} from './componenets/Auth/Login';
-import Register from './componenets/Auth/Register';
+import { Login } from './componenets/Auth/Login'
+import Register from './componenets/Auth/Register'
 
 const root = ReactDOMClient.createRoot(document.getElementById('root'))
 root.render(
@@ -17,9 +17,8 @@ root.render(
       <Route path="/" element={<Index />}></Route>
       <Route path="/auth" element={<Login />}></Route>
       <Route path="/auth/register" element={<Register />}></Route>
-
     </Routes>
-  </BrowserRouter>
+  </BrowserRouter>,
 )
 
 // If you want to start measuring performance in your app, pass a function
