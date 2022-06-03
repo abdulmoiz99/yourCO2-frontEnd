d7f09795598de3bdf512fb0315026b809fa473df
Abdul Moiz <moiza0799@gmail.com>
2022-06-03 23:00:19 +0500

    Updated Maps

--- src/views/Index.js
+++ src/views/Index.js
@@ -49,75 +49,13 @@ export default function Index() {
           alt=".."
         />
       </section>
-      <section className="mt-48 md:mt-40 pb-40 relative bg-blueGray-100">
+      <section className="mt-1 md:mt-1 pb-1 relative bg-blueGray-100">
+        
         <div className="container mx-auto px-4 pb-32 pt-48">
           <div className="items-center flex flex-wrap">
             <div className="w-full md:w-5/12 ml-auto px-12 md:px-4">
               <div className="md:pr-12">
-                <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
-                  <FontAwesomeIcon icon={faChartArea} />
-                </div>
-                <h3 className="text-3xl font-semibold">Complex Calculations</h3>
-                <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
-                  This extension comes a lot of fully coded examples that help
-                  you get started faster. You can adjust the colors and also the
-                  carbon factor.
-                </p>
-                <ul className="list-none mt-6">
-                  <li className="py-2">
-                    <div className="flex items-center">
-                      <div>
-                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-50 mr-3">
-                          <FontAwesomeIcon icon={faPaperPlane} />
-                        </span>
-                      </div>
-                      <div>
-                        <h4 className="text-blueGray-500">
-                          Built by Developers for Developers
-                        </h4>
-                      </div>
-                    </div>
-                  </li>
-                  <li className="py-2">
-                    <div className="flex items-center">
-                      <div>
-                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-50 mr-3">
-                          <FontAwesomeIcon icon={faPaperPlane} />
-                        </span>
-                      </div>
-                      <div>
-                        <h4 className="text-blueGray-500">
-                          Carefully crafted code for Calculations
-                        </h4>
-                      </div>
-                    </div>
-                  </li>
-                  <li className="py-2">
-                    <div className="flex items-center">
-                      <div>
-                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-50 mr-3">
-                          <i></i>
-                        </span>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </div>
-
-            <div className="w-full md:w-6/12 mr-auto px-0 pt-24 md:pt-0">
-              <CardLineChart />
-            </div>
-          </div>
-        </div>
-        <div className="container mx-auto px-4 pb-32 pt-48">
-          <div className="items-center flex flex-wrap">
-            <div className="w-full md:w-5/12 ml-auto px-12 md:px-4">
-              <div className="md:pr-12">
-                <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
-                  <FontAwesomeIcon icon={faChartArea} />
-                </div>
-                <h3 className="text-3xl font-semibold">Complex Map</h3>
+                <h3 className="text-3xl font-semibold">Real time carbon emissions of electricity in your region</h3>
                 <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
                   This extension comes a lot of fully coded examples that help
                   you get started faster. You can adjust the colors and also the
@@ -173,6 +111,72 @@ export default function Index() {
             </div>
           </div>
         </div>
+
+
+        <div className="container mx-auto px-4 pb-32 pt-30">
+          <div className="items-center flex flex-wrap">
+            <div className="w-full md:w-5/12 ml-auto px-12 md:px-4">
+              <div className="md:pr-12">
+                <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-1 mb-6 shadow-lg rounded-full bg-white">
+                  <FontAwesomeIcon icon={faChartArea} />
+                </div>
+                <h3 className="text-3xl font-semibold">See the last months regional electrical emissions data vs the static emissions commonly used</h3>
+                <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
+                  This extension comes a lot of fully coded examples that help
+                  you get started faster. You can adjust the colors and also the
+                  carbon factor.
+                </p>
+                <ul className="list-none mt-6">
+                  <li className="py-2">
+                    <div className="flex items-center">
+                      <div>
+                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-50 mr-3">
+                          <FontAwesomeIcon icon={faPaperPlane} />
+                        </span>
+                      </div>
+                      <div>
+                        <h4 className="text-blueGray-500">
+                          Built by Developers for Developers
+                        </h4>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="py-2">
+                    <div className="flex items-center">
+                      <div>
+                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-50 mr-3">
+                          <FontAwesomeIcon icon={faPaperPlane} />
+                        </span>
+                      </div>
+                      <div>
+                        <h4 className="text-blueGray-500">
+                          Carefully crafted code for Calculations
+                        </h4>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="py-2">
+                    <div className="flex items-center">
+                      <div>
+                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-50 mr-3">
+                          <i></i>
+                        </span>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="w-full md:w-6/12 mr-auto px-0 pt-24 md:pt-0">
+              <CardLineChart />
+            </div>
+          </div>
+        </div>
+
+
+
+
         <div className="justify-center text-center flex flex-wrap mt-24">
           <div className="w-full md:w-6/12 px-12 md:px-4">
             <h2 className="font-semibold text-4xl">Beautiful Example Pages</h2>
