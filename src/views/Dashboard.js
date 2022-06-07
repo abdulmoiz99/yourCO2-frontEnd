b44bdb41c935de1c220d1af053028f08e851ce5d
Abdul Moiz <moiza0799@gmail.com>
2022-06-07 21:14:01 +0500

    Fetch reponse from API for report data

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -6,13 +6,34 @@ import HeaderStats from '../componenets/Headers/HeaderStats'
 import CardProfile from '../componenets/Cards/CardProfile'
 import { CardSettings } from '../componenets/Cards/CardSettings'
 import FooterAdmin from '../componenets/Footers/FooterAdmin'
+import { getStorage } from '../shared/LoacalStorage'
 
 export class Dashboard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { report: [], loading: true }
+  }
+  componentDidMount() {
+    this.populateGraphData()
+  }
+  populateGraphData = async () => {
+    let token = getStorage('token')
+    const response = await fetch(
+      'https://youco2api.azurewebsites.net/api/Business/reports',
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      },
+    )
+    const data = await response.json()
+    this.setState({
+      report: data,
+      loading: false,
+    })
+  }
   render() {
     return (
       <>
         <Sidebar />
-
         <div className="relative md:ml-64 bg-blueGray-100">
           <NavBar />
           {/* Header */}
@@ -21,7 +42,10 @@ export class Dashboard extends React.Component {
           <div className="px-4 md:px-10 mx-auto w-full -m-24">
             <div className="flex flex-wrap">
               <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-                <CarbonGraph />
+                <CarbonGraph
+                  reportData={this.state.report}
+                  loading={this.state.loading}
+                />
               </div>
               <div className="w-full xl:w-4/12 px-4">
                 <CardProfile />
